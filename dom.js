1cbb06899357fa5702c102b89e056889caf41dc2
atiqur rahman <atiq95913@gmail.com>
2023-03-29 16:59:00 +0600

    something add

---
+++ dom.js
@@ -0,0 +1,22 @@
+
+
+document.getElementById("btn").addEventListener("click",()=>{
+document.getElementsByTagName("h2")[0].style.color="red"
+document.getElementsByTagName("h2")[1].style.color="green"
+document.getElementsByTagName("h2")[2].style.color="black"
+document.getElementsByTagName("h2")[3].style.color="blue"
+document.getElementsByTagName("h2")[4].style.color="tomato"
+document.getElementsByClassName("c")[4].style.backgroundColor="tomato"
+document.getElementsByClassName("c")[0].style.backgroundColor="red"
+document.getElementsByClassName("c")[1].style.backgroundColor="green"
+document.getElementsByClassName("c")[2].style.backgroundColor="gold"
+document.getElementsByClassName("c")[3].style.backgroundColor="black"
+})
+
+
+
+document.querySelector("h2").style.backgroundColor="green"
+document.querySelector("#header").style.backgroundColor="blue"
+document.querySelector(".c").style.backgroundColor="Yellow"
+document.querySelector("#u li a").style.color="red"
+document.querySelectorAll("h3")[4].style.color="tomato"
---
+++ event.js
@@ -0,0 +1,9 @@
+
+const message =()=>{
+	const head = document.getElementById("h")
+	head.innerHTML="Hello World ARE KNOW I DeVeloper"
+	head.style.color="red"
+	head.style.backgroundColor="green"
+}
+
+
